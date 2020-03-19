097db6ed0a3b3cb23a21a0aa5d4ebc2f5bcf52f6
Jonathan Tang <hello@jtang.dev>
2020-03-19 16:52:58 +0000

    Setup basic NodeJS Express server

---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+const port = 9000;
+
+app.get('/', (_req, res) => res.send('Hello World!'));
+app.listen(port);
+
+console.log(`[app]: http://localhost:${port}`);

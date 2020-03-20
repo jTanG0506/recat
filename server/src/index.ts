92a5ea34b037e33d4bb82f26dddde6ee5f220401
Jonathan Tang <hello@jtang.dev>
2020-03-20 10:59:43 +0000

    Create simple GraphQL API

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs, resolvers } from "./graphql"
 
 const app = express();
+const server = new ApolloServer({ typeDefs, resolvers });
 const port = 9000;
 
-app.get('/', (_req, res) => res.send('Hello World!'));
+server.applyMiddleware({ app, path: "/api" });
+
 app.listen(port);
 
 console.log(`[app]: http://localhost:${port}`);

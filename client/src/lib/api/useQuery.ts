96e46885912c2ee0d34a1ef9ebdda84cd2cf392e
Jonathan Tang <hello@jtang.dev>
2020-03-29 22:35:20 +0100

    Decouple state updates from actions using useReducer hooks

--- client/src/lib/api/useQuery.ts
+++ client/src/lib/api/useQuery.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useReducer } from "react";
 import { server } from "./server"
 
 interface State<TData> {
@@ -11,8 +11,28 @@ interface QueryResult<TData> extends State<TData> {
   refetch: () => void;
 }
 
+type Action<TData> =
+  | { type: "FETCH" }
+  | { type: "FETCH_SUCCESS", payload: TData }
+  | { type: "FETCH_ERROR" };
+
+const reducer = <TData>() => (state: State<TData>, action: Action<TData>): State<TData> => {
+  switch (action.type) {
+    case "FETCH":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, data: action.payload, loading: false, error: false };
+      case "FETCH_ERROR":
+        return { ...state, loading: false, error: true };
+      default:
+        throw new Error();
+  }
+}
+
 export const useQuery = <TData = any>(query: string): QueryResult<TData> => {
-  const [state, setState] = useState<State<TData>>({
+
+  const fetchReducer = reducer<TData>();
+  const [state, dispatch] = useReducer(fetchReducer, {
     data: null,
     loading: false,
     error: false
@@ -21,17 +41,16 @@ export const useQuery = <TData = any>(query: string): QueryResult<TData> => {
   const fetch = useCallback(() => {
     const fetchApi = async () => {
       try {
-        setState({ data: null, loading: true, error: false });
+        dispatch({ type: "FETCH" });
         const { data, errors } = await server.fetch<TData>({ query });
 
         if (errors && errors.length) {
           throw new Error(errors[0].message);
         }
 
-        setState({ data, loading: false, error: false });
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
       } catch (err) {
-        setState({ data: null, loading: false, error: true });
-        throw console.error(err);
+        dispatch({ type: "FETCH_ERROR" });
       }
     };
 

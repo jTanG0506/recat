7336c8c4d7961083982ac7a6f8f35f60e1d729d4
Jonathan Tang <hello@jtang.dev>
2020-03-30 21:24:10 +0100

    Create types for booking, listing and user

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,17 +1,61 @@
 import { ObjectId, Collection } from "mongodb";
 
+export interface Booking {
+  _id: ObjectId;
+  listing: ObjectId;
+  tenant: string;
+  checkIn: string;
+  checkOut: string;
+}
+
+interface BookingsIndexMonth {
+  [key: string]: boolean;
+}
+
+interface BookingsIndexYear {
+  [key: string]: BookingsIndexMonth;
+}
+
+export interface BookingsIndex {
+  [key: string]: BookingsIndexYear;
+}
+
 export interface Listing {
   _id: ObjectId;
   title: string;
+  description: string;
   image: string;
+  host: string;
+  type: ListingType;
   address: string;
+  country: string;
+  admin: string;
+  city: string;
+  bookings: ObjectId[];
+  bookingsIndex: BookingsIndex;
   price: number;
   numOfGuests: number;
-  numOfBeds: number;
-  numOfBaths: number;
-  rating: number;
+}
+
+export enum ListingType {
+  Apartment = "APARTMENT",
+  House = "HOUSE"
+}
+
+export interface User {
+  _id: string;
+  token: string;
+  name: string;
+  avatar: string;
+  contact: string;
+  walletId?: string;
+  income: number;
+  bookings: ObjectId[];
+  listings: ObjectId[];
 }
 
 export interface Database {
+  bookings: Collection<Booking>;
   listings: Collection<Listing>;
+  users: Collection<User>;
 }

1d08d3f5a10ee7176de281523b737770e0d4d26f
Jonathan Tang <hello@jtang.dev>
2020-03-30 21:58:51 +0100

    Create additional seeding data and create clear script

---
+++ server/temp/clear.ts
@@ -0,0 +1,30 @@
+require("dotenv").config();
+import { connectDatabase } from "../src/database";
+
+const clear = async () => {
+  try {
+    console.log("[clear]: running...");
+
+    const db = await connectDatabase();
+
+    const bookings = await db.bookings.find({}).toArray();
+    const listings = await db.bookings.find({}).toArray();
+    const users = await db.users.find({}).toArray();
+
+    if (bookings.length > 0) {
+      await db.bookings.drop();
+    }
+
+    if (listings.length > 0) {
+      await db.listings.drop();
+    }
+
+    if (users.length > 0) {
+      await db.users.drop();
+    }
+
+    console.log("[clear]: success");
+  } catch {
+    throw new Error("Failed to clear database");
+  }
+}

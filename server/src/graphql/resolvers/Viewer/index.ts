8ffe5a93f20353473bcb9781f17949e8286ad9fb
Jonathan Tang <hello@jtang.dev>
2020-04-01 03:42:30 +0100

    Add server sided viewer cookies

--- server/src/graphql/resolvers/Viewer/index.ts
+++ server/src/graphql/resolvers/Viewer/index.ts
@@ -1,10 +1,24 @@
+import { Request, Response } from "express";
 import { IResolvers } from "apollo-server-express";
 import { Google } from "../../../lib/api";
 import { Viewer, Database, User } from "../../../lib/types";
 import { LogInArgs } from "./types";
 import crypto from "crypto";
 
-const logInViaGoogle = async (code: string, token: string, db: Database): Promise<User | undefined> => {
+const cookieOptions = {
+  httpOnly: true,
+  sameSite: true,
+  signed: true,
+  secure: process.env.NODE_ENV === "development" ? false : true
+}
+
+const logInViaGoogle = async (
+  code: string,
+  token: string,
+  db: Database,
+  res: Response
+): Promise<User | undefined> => {
+
   const { user } = await Google.login(code);
 
   if (!user) {
@@ -58,8 +72,34 @@ const logInViaGoogle = async (code: string, token: string, db: Database): Promis
     viewer = insertRes.ops[0];
   }
 
+  res.cookie("viewer", userId, {
+    ...cookieOptions,
+    maxAge: 365 * 24 * 60 * 60 * 1000
+  });
+
   return viewer;
 }
+
+const logInViaCookie = async (
+  token: string,
+  db: Database,
+  req: Request,
+  res: Response
+): Promise<User | undefined> => {
+  const updateRes = await db.users.findOneAndUpdate(
+    { _id: req.signedCookies.viewer },
+    { $set: { token } },
+    { returnOriginal: false }
+  );
+
+  let viewer = updateRes.value;
+
+  if (!viewer) {
+    res.clearCookie("viewer", cookieOptions);
+  }
+
+  return viewer;
+};
 
 export const viewerResolvers: IResolvers = {
   Query: {
@@ -72,14 +112,18 @@ export const viewerResolvers: IResolvers = {
     }
   },
   Mutation: {
-    logIn: async (_root: undefined, { input }: LogInArgs, { db }: { db: Database }): Promise<Viewer> => {
+    logIn: async (
+      _root: undefined,
+      { input }: LogInArgs,
+      { db, req, res }: { db: Database, req: Request, res: Response }
+    ): Promise<Viewer> => {
       try {
         const code = input ? input.code : null;
         const token = crypto.randomBytes(16).toString("hex");
 
         const viewer: User | undefined = code
-          ? await logInViaGoogle(code, token, db)
-          : undefined;
+          ? await logInViaGoogle(code, token, db, res)
+          : await logInViaCookie(token, db, req, res);
 
         if (!viewer) {
           return { didRequest: true };
@@ -96,9 +140,9 @@ export const viewerResolvers: IResolvers = {
         throw new Error(`Failed to login: ${error}`);
       }
     },
-    logOut: (): Viewer => {
+    logOut: (_root: undefined, _args: {}, { res }: { res: Response }): Viewer => {
       try {
-        // TODO: Implement logout via cookies.
+        res.clearCookie("viewer", cookieOptions);
         return { didRequest: true };
       } catch (error) {
         throw new Error(`Failed to logout: ${error}`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,17 @@
 require("dotenv").config();
 import express, { Application } from "express";
+import cookieParser from "cookie-parser";
 import { ApolloServer } from "apollo-server-express";
 import { connectDatabase } from "./database";
 import { typeDefs, resolvers } from "./graphql"
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
+  app.use(cookieParser(process.env.SECRET));
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => ({ db })
+    context: ({ req, res }) => ({ db, req, res })
   });
 
   server.applyMiddleware({ app, path: "/api" });

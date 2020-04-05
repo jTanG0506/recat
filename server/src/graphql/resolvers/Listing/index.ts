c17e40a80ea20b4e10c9c86724c6153d7b627d86
Jonathan Tang <hello@jtang.dev>
2020-04-05 18:06:13 +0100

    Implement GraphQL resolvers for Listings

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -1,10 +1,81 @@
 import { IResolvers } from "apollo-server-express";
-import { Listing } from "../../../lib/types";
+import { Request } from "express";
+import { Database, Listing, User } from "../../../lib/types";
+import { ListingArgs, ListingBookingsArgs, ListingBookingsData } from "./types";
+import { authorize } from "../../../lib/utils";
+import { ObjectId } from "mongodb";
 
 export const listingResolvers: IResolvers = {
+  Query: {
+    listing: async (
+      _root: undefined,
+      { id }: ListingArgs,
+      { db, req }: { db: Database, req: Request }
+    ): Promise<Listing> => {
+      try {
+        const listing = await db.listings.findOne({ _id: new ObjectId(id) });
+        if (!listing) {
+          throw new Error(`Listing can't be found!`);
+        }
+
+        const viewer = await authorize(db, req);
+        if (viewer && viewer._id === listing.host) {
+          listing.authorized = true;
+        }
+
+        return listing;
+      } catch (error) {
+        throw new Error(`Failed to query listing: ${error}`);
+      }
+    }
+  },
   Listing: {
     id: (listing: Listing): string => {
       return listing._id.toString();
+    },
+    host: async (
+      listing: Listing,
+      _args: {},
+      { db }: { db: Database }
+    ): Promise<User> => {
+      const host = await db.users.findOne({ _id: listing.host });
+      if (!host) {
+        throw new Error("Host cannot be found");
+      }
+
+      return host;
+    },
+    bookingsIndex: (listing: Listing): string => {
+      return JSON.stringify(listing.bookingsIndex);
+    },
+    bookings: async (
+      listing: Listing,
+      { limit, page }: ListingBookingsArgs,
+      { db, req }: { db: Database, req: Request }
+    ): Promise<ListingBookingsData | null> => {
+      try {
+        if (!listing.authorized) {
+          return null;
+        }
+
+        const data: ListingBookingsData = {
+          total: 0,
+          result: []
+        }
+
+        let cursor = await db.bookings.find({
+          _id: { $in: listing.bookings }
+        })
+          .skip(page > 0 ? (page - 1) * limit : 0)
+          .limit(limit);
+
+        data.total = await cursor.count();
+        data.result = await cursor.toArray();
+
+        return data;
+      } catch (error) {
+        throw new Error(`Failed to query listing bookings: ${error}`);
+      }
     }
   }
 }
--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -43,6 +43,7 @@ export interface Listing {
   bookingsIndex: BookingsIndex;
   price: number;
   numOfGuests: number;
+  authorized?: boolean;
 }
 
 export enum ListingType {

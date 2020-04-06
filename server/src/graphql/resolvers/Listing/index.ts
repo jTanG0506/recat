7e9eac7d6e8398c7fc9c2c33dc28e5552e3e6601
Jonathan Tang <hello@jtang.dev>
2020-04-06 22:17:11 +0100

    Implement listings resolver for price based queries

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -1,7 +1,14 @@
 import { IResolvers } from "apollo-server-express";
 import { Request } from "express";
 import { Database, Listing, User } from "../../../lib/types";
-import { ListingArgs, ListingBookingsArgs, ListingBookingsData } from "./types";
+import {
+  ListingArgs,
+  ListingBookingsArgs,
+  ListingBookingsData,
+  ListingsArgs,
+  ListingsData,
+  ListingsFilter
+} from "./types";
 import { authorize } from "../../../lib/utils";
 import { ObjectId } from "mongodb";
 
@@ -26,6 +33,34 @@ export const listingResolvers: IResolvers = {
         return listing;
       } catch (error) {
         throw new Error(`Failed to query listing: ${error}`);
+      }
+    },
+    listings: async (
+      _root: undefined,
+      { filter, limit, page }: ListingsArgs,
+      { db }: { db: Database }
+    ): Promise<ListingsData> => {
+      try {
+        const data: ListingsData = {
+          total: 0,
+          result: []
+        };
+
+        let cursor = await db.listings.find({});
+
+        if (filter) {
+          let sortDirection = filter === ListingsFilter.PRICE_LOW_TO_HIGH ? 1 : -1;
+          cursor = cursor.sort({ price: sortDirection });
+        }
+
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0).limit(limit);
+
+        data.total = await cursor.count();
+        data.result = await cursor.toArray();
+
+        return data;
+      } catch (error) {
+        throw new Error(`Failed to query listings: ${error}`);
       }
     }
   },
--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -19,6 +19,11 @@ export const typeDefs = gql`
     HOUSE
   }
 
+  enum ListingsFilter {
+    PRICE_LOW_TO_HIGH
+    PRICE_HIGH_TO_LOW
+  }
+
   type Listing {
     id: ID!
     title: String!
@@ -67,6 +72,7 @@ export const typeDefs = gql`
     authUrl: String!
     user(id: ID!): User!
     listing(id: ID!): Listing!
+    listings(filter: ListingsFilter!, limit: Int!, page: Int!): Listings!
   }
 
   type Mutation {

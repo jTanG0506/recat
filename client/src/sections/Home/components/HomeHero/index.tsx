1980e72e51e23b5069829c9702429c68bae0fbea
Jonathan Tang <hello@jtang.dev>
2020-04-07 00:13:49 +0100

    Create basic homepage with HomeHero component

---
+++ client/src/sections/Home/components/HomeHero/index.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Card, Col, Input, Row, Typography } from "antd";
+
+import torontoImage from "../../assets/toronto.jpg";
+import dubaiImage from "../../assets/dubai.jpg";
+import losAngelesImage from "../../assets/los-angeles.jpg";
+import londonImage from "../../assets/london.jpg";
+import { Link } from "react-router-dom";
+
+const { Title } = Typography;
+const { Search } = Input;
+
+interface Props {
+  onSearch: (value: string) => void;
+}
+
+export const HomeHero = ({ onSearch }: Props) => {
+  return (
+    <div className="home-hero">
+      <div className="home-hero__search">
+        <Title className="home-hero__title">Find a place you'll love to stay at</Title>
+        <Search
+          placeholder="Where would you like to stay?"
+          size="large"
+          enterButton
+          className="home-hero__search"
+          onSearch={onSearch}
+        />
+      </div>
+      <Row gutter={12} className="home-hero__cards">
+        <Col xs={0} md={6}>
+          <Link to="/listings/london">
+            <Card cover={<img alt="London" src={londonImage} />}>London</Card>
+          </Link>
+        </Col>
+        <Col xs={12} md={6}>
+          <Link to="/listings/dubai">
+            <Card cover={<img alt="Dubai" src={dubaiImage} />}>Dubai</Card>
+          </Link>
+        </Col>
+        <Col xs={0} md={6}>
+          <Link to="/listings/los%20angeles">
+            <Card cover={<img alt="Los Angeles" src={losAngelesImage} />}>Los Angeles</Card>
+          </Link>
+        </Col>
+        <Col xs={12} md={6}>
+          <Link to="/listings/toronto">
+            <Card cover={<img alt="Toronto" src={torontoImage} />}>Toronto</Card>
+          </Link>
+        </Col>
+      </Row>
+    </div>
+  )
+}
--- client/src/sections/Home/index.tsx
+++ client/src/sections/Home/index.tsx
@@ -1,5 +1,62 @@
 import React from "react";
+import { Link, RouteComponentProps } from "react-router-dom";
+import { Col, Row, Layout, Typography } from "antd";
+import { HomeHero } from "./components";
+import { displayErrorMessage } from "../../lib/utils";
 
-export const Home = () => {
-  return <h2>Home</h2>
+import mapBackground from "./assets/map-background.jpg";
+import sanFransiscoImage from "./assets/san-fransisco.jpg";
+import cancunImage from "./assets/cancun.jpg";
+
+const { Content } = Layout;
+const { Paragraph, Title } = Typography;
+
+export const Home = ({ history }: RouteComponentProps) => {
+  const onSearch = (value: string) => {
+    const trimmedValue = value.trim();
+
+    if (trimmedValue) {
+      history.push(`/listings/${trimmedValue}`);
+    } else {
+      displayErrorMessage("Please enter a valid search!");
+    }
+  };
+
+  return (
+    <Content className="home" style={{ backgroundImage: `url(${mapBackground})` }}>
+      <HomeHero onSearch={onSearch} />
+
+      <div className="home__cta-section">
+        <Title level={2} className="home__cta-section-title">
+          Your guide for all things rental
+        </Title>
+        <Paragraph>Helping you make the best decisions in renting your last minute locations.</Paragraph>
+        <Link to="/listings/united%20kingdom" className="ant-btn ant-btn-primary ant-btn-lg home__cta-section-button">
+          Popular listings in the United Kingdom
+        </Link>
+      </div>
+
+      <div className="home__listings">
+        <Title level={4} className="home__listings-title">
+          Listings of any kind
+        </Title>
+        <Row gutter={12}>
+          <Col xs={24} sm={12}>
+            <Link to="/listings/san%20fransisco">
+              <div className="home__listings-img-cover">
+                <img src={sanFransiscoImage} alt="San Fransisco" className="home__listings-img" />
+              </div>
+            </Link>
+          </Col>
+          <Col xs={24} sm={12}>
+            <Link to="/listings/cancún">
+              <div className="home__listings-img-cover">
+                <img src={cancunImage} alt="Cancún" className="home__listings-img" />
+              </div>
+            </Link>
+          </Col>
+        </Row>
+      </div>
+    </Content>
+  )
 };

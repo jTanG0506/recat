8f50a0a8d7e83ceddb21ce8ebc37ac2c4315aee1
Jonathan Tang <hello@jtang.dev>
2020-04-09 17:38:18 +0100

    Use body-parser package to increase body size limit of requests

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 require("dotenv").config();
 import express, { Application } from "express";
+import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import { ApolloServer } from "apollo-server-express";
 import { connectDatabase } from "./database";
@@ -7,6 +8,7 @@ import { typeDefs, resolvers } from "./graphql"
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
+  app.use(bodyParser.json({ limit: "2mb" }));
   app.use(cookieParser(process.env.SECRET));
   const server = new ApolloServer({
     typeDefs,

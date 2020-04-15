afa6db6f061e0d0395fba153b35925f9d1cbdeff
Jonathan Tang <hello@jtang.dev>
2020-04-15 14:45:44 +0100

    Add compression library to use server to serve client files too

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,14 +2,20 @@ require("dotenv").config();
 import express, { Application } from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import compression from "compression";
 import { ApolloServer } from "apollo-server-express";
 import { connectDatabase } from "./database";
-import { typeDefs, resolvers } from "./graphql"
+import { typeDefs, resolvers } from "./graphql";
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
   app.use(bodyParser.json({ limit: "2mb" }));
   app.use(cookieParser(process.env.SECRET));
+  app.use(compression());
+
+  app.use(express.static(`${__dirname}/client`));
+  app.get("/*", (_req, res) => res.sendFile(`${__dirname}/client/index.html`));
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
